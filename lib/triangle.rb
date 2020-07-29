7e97aebd6454268d1dcb00ea2994451b2914ab12
William Han Findiesen <william.findiesen@gmail.com>
2020-07-29 07:12:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     #error for length if 0 or less 
     raise TriangleError if (@side1 <=0) || (@side2 <=0) || (@side3 <=0)
     #error for length if 2 sides longer than the third
-    raise TriangleError if (@side1 + @side2 > @side3) || (@side1 + @side3 > @side2) || (@side2 + @side3 > @side1)
+    raise TriangleError if (@side1 + @side2 >= @side3) || (@side1 + @side3 >= @side2) || (@side2 + @side3 >= @side1)
     return :equilateral if (@side1 == @side2) && (@side2 == @side3) && (@side1 == @side3)
     return :isosceles if (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
     return :scalene if (@side1 != @side2) || (@side2 != @side3) || (@side1 != @side3)
